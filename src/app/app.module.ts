88b02a35922f6daf1f2d1064125365b10de1b33e
jgascona <josegascon@quibim.com>
2023-12-18 20:31:22 +0100

    first map implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GoogleMapsModule } from '@angular/google-maps';
+import { MapComponent } from './map/map.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, MapComponent, NavbarComponent],
+  imports: [BrowserModule, GoogleMapsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/map/map.component.ts
@@ -0,0 +1,58 @@
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit, Renderer2 } from '@angular/core';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss'],
+})
+export class MapComponent implements OnInit {
+  markerOptions: google.maps.MarkerOptions = { draggable: false };
+  position: google.maps.LatLngLiteral = { lat: 39.5, lng: -0.393 };
+  center = { lng: -0.392787, lat: 39.467 };
+  zoom = 13;
+  options: google.maps.MapOptions = {
+    zoomControl: true,
+    scrollwheel: true,
+    disableDoubleClickZoom: true,
+    maxZoom: 15,
+    minZoom: 8,
+    disableDefaultUI: true,
+    styles: [
+      {
+        featureType: 'poi',
+        stylers: [{ visibility: 'off' }],
+      },
+    ],
+  };
+  spots: Spot[] = [
+    { position: { lat: 39.49, lng: -0.3763 }, label: 'A', title: 'Oro' },
+    { position: { lat: 39.4697, lng: -0.3774 }, label: 'B', title: 'Plata' },
+    { position: { lat: 39.44, lng: -0.376 }, label: 'C', title: 'Bronce' },
+  ];
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private renderer2: Renderer2
+  ) {}
+
+  ngOnInit() {}
+
+  private loadScript(url: any) {
+    return new Promise((resolve, reject) => {
+      const script = this.renderer2.createElement('script');
+      script.type = 'text/javascript';
+      script.src = url;
+      script.text = ``;
+      script.onload = resolve;
+      script.onerror = reject;
+      this.renderer2.appendChild(this.document.head, script);
+    });
+  }
+}
+
+interface Spot {
+  position: google.maps.LatLngLiteral;
+  label: string;
+  title: string;
+}
