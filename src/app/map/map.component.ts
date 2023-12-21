9af1186203533d8a345d7463af78a78cf9dcc98b
jgascona <josegascon@quibim.com>
2023-12-21 12:51:35 +0100

    Increase number of mock points

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,4 @@
-import { DOCUMENT } from '@angular/common';
-import { Component, Inject, OnInit, Renderer2 } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { mapStyles } from '../config/mapConfig';
 import { Spot, spots } from '../config/spots';
 
@@ -8,8 +7,10 @@ import { Spot, spots } from '../config/spots';
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss'],
 })
-export class MapComponent implements OnInit {
-  spots: Spot[] = spots;
+export class MapComponent implements AfterViewInit {
+  @ViewChild('map') map!: google.maps.Map;
+  markers: Spot[] = spots;
+
   position: google.maps.LatLngLiteral = { lat: 39.5, lng: -0.393 };
   center = { lng: -0.392787, lat: 39.467 };
   zoom = 14;
@@ -23,39 +24,7 @@ export class MapComponent implements OnInit {
     styles: mapStyles,
   };
 
-  markerPosition = { lat: -34.397, lng: 150.644 };
-  marker = new google.maps.Marker({
-    position: this.markerPosition,
-    // map: map, // Asigna el mapa en el que se colocará el marcador
-    icon: {
-      path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW, // Puedes usar otros símbolos predeterminados
-      fillColor: '#DDDDDD', // Establece el color del marcador
-      fillOpacity: 1, // Establece la opacidad del color del marcador (de 0 a 1)
-      strokeWeight: 0, // No hay borde,
-      scaledSize: new google.maps.Size(10, 10),
-    },
-    title: 'Mi marcador', // Título del marcador (puedes omitir esto si no deseas un título)
-  });
-
-  // Define el color que deseas para el marcador (en formato hexadecimal)
-  // var markerColor = '#FF0000'; // Por ejemplo, rojo
-
-  constructor(
-    @Inject(DOCUMENT) private document: Document,
-    private renderer2: Renderer2
-  ) {}
-
-  ngOnInit() {}
-
-  private loadScript(url: any) {
-    return new Promise((resolve, reject) => {
-      const script = this.renderer2.createElement('script');
-      script.type = 'text/javascript';
-      script.src = url;
-      script.text = ``;
-      script.onload = resolve;
-      script.onerror = reject;
-      this.renderer2.appendChild(this.document.head, script);
-    });
+  ngAfterViewInit() {
+    console.log(this.map);
   }
 }
